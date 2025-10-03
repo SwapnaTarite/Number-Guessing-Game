b75e520d9bf017585bbc47c678873d0972c3341f
SwapnaTarite <swapnatarite11@gmail.com>
2025-10-03 14:33:11 +0530

    Add files via upload

---
+++ NumberGuess2.py
@@ -0,0 +1,23 @@
+'''Allow the user to specify the minimum and maximum values for the number
+range before the game starts. This gives the player more control over the
+difficulty level'''
+import random
+counter=0
+Number=random.randint(25,50)
+while True:
+    Guess=int(input("Guess the number :\n min 25  and maximum 50 :"))
+    counter=counter+1
+    print(Number)
+    if Guess==Number:
+            print(f"Congratulations! You Guessed the number in {counter} attempts .")
+            break
+    elif Guess<Number:
+            print("Too low!  Try again")
+    elif Guess>Number:
+            print("Too High! Try again")
+    else:
+        pass
+
+
+
+          
